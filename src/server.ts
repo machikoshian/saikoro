d65afa02fd14746bd419ded4a7e94d7cfd2f3d11
Koshian <koshian.machikoro@gmail.com>
2017-04-29 01:59:38 +0900

    Added http server with Node.js

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,25 @@
 import { Dice } from "./dice";
 
-console.log(Dice.roll(2, 7).debugString());
+declare function require(name:string):any;
+var http = require("http");
+
+class Main {
+  constructor() {
+    let server = http.createServer();
+    server.on('request', (request, response) => this.requestHandler(request, response));
+    server.listen("3156");
+  }
+
+  private requestHandler(request, response):void {
+    console.log(request.url);
+    console.log(request.method);
+    if (request.url == "/dice") {
+      let output:string = Dice.roll(2, 7).debugString();
+      console.log(output);
+      response.end(output);
+    }
+  }
+}
+let main:Main = new Main();
+
+console.log('Server running at http://localhost:3156/');
