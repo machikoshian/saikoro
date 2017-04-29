a22d7c2ad77e5e6713219ff9be8e78c5851fa618
Koshian <koshian.machikoro@gmail.com>
2017-04-29 11:43:01 +0900

    Enabled to set a location of a facility.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Board,Facility } from "./board";
 
 declare function require(name:string):any;
 var http = require("http");
+var url = require("url");
 
 class Main {
   private board:Board;
@@ -18,19 +19,27 @@ class Main {
   private requestHandler(request, response):void {
     console.log(request.url);
     console.log(request.method);
-    if (request.url == "/dice") {
+
+    let url_parts = url.parse(request.url, true);
+    let query = url_parts.query;
+    let pathname:string = url_parts.pathname;
+    console.log(url_parts.query);
+
+    if (pathname == "/dice") {
       let output:string = Dice.roll(2, 7).debugString();
       console.log(output);
       response.end(output);
-    } else if (request.url == "/board") {
+    } else if (pathname == "/board") {
       let output:string = this.board.debugString();
       console.log(output);
       response.end(output);
-    } else if (request.url == "/build") {
-      let x:number = 3;
-      let y:number = 2;
-      let facility:Facility = new Facility("[Bee]");
-      this.board.setFacility(x, y, facility);
+    } else if (pathname == "/build") {
+      let x:number = query.x;
+      let y:number = query.y;
+      if (x && y) {
+        let facility:Facility = new Facility("[Bee]");
+        this.board.setFacility(x, y, facility);
+      }
 
       let output:string = this.board.debugString();
       console.log(output);
