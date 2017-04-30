7e8d945ce072dff9418db0daefb6a3e4f3727d84
Koshian <koshian.machikoro@gmail.com>
2017-04-30 14:40:01 +0900

    Added drawBoard.

--- src/saikoro.ts
+++ src/saikoro.ts
@@ -34,26 +34,42 @@ function callbackDice(response:string):void {
 
 function callbackBoard(response:string):void {
   let board:Board = Board.fromJSON(JSON.parse(response));
-  let output:string = "<table>";
   for (let y:number = 0; y < board.row; ++y) {
-    output += "<tr>";
     for (let x:number = 0; x < board.column; ++x) {
       let field:Field = board.fields[x][y];
       let name:string = field.facility ? field.facility.name : "";
-      output += "<td>" + name + "</td>";
+      document.getElementById(`field_${x}_${y}`).innerHTML = name;
+    }
+  }
+}
+
+function drawBoard(column:number=12, row:number=5):void {
+  let output:string = "<table>";
+  for (let y:number = 0; y < row; ++y) {
+    output += "<tr>";
+    for (let x:number = 0; x < column; ++x) {
+      output += `<td id="field_${x}_${y}"></td>`;
     }
     output += "</tr>";
   }
 
   output += "<tr>";
-  for (let x:number = 1; x <= board.column; ++x) {
-    output += "<th>" + x + "</th>";
+  for (let x:number = 1; x <= column; ++x) {
+    output += `<th>${x}</th>`;
   }
   output += "</tr>";
 
   output += "</table>";
   document.getElementById("board").innerHTML = output;
+
+  // Add click listeners.
+  for (let y:number = 0; y < row; ++y) {
+    for (let x:number = 0; x < column; ++x) {
+      document.getElementById(`field_${x}_${y}`).addEventListener("click", ()=>{alert("yay");});
+    }
+  }
 }
 
 // HttpRequest.Send("/dice?dice_num=2&aim=7", callbackDice);
+drawBoard();
 HttpRequest.Send("/board", callbackBoard);
